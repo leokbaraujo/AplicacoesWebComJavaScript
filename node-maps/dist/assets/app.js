eba6c8664de343e8de70963881af6c77e7967a46
Leonel Araujo <leonel@gera3.com.br>
2018-04-14 11:16:35 -0300

    Finalizando a aplicacao de acesso a mapas

--- node-maps/dist/assets/app.js
+++ node-maps/dist/assets/app.js
@@ -1,6 +1,7 @@
 window.onload = () => {
     let grid = document.querySelector("#grid");
     let button = document.querySelector("#send");
+    button.addEventListener('click', save());
     read();
 };
 
@@ -30,3 +31,35 @@ function read() {
             console.error(`Erro ao executar leitura dos cards. ${error}`);
         });
 };
+
+function save() {
+    if (!navigator.geolocation) {
+        alert('Seu browser não suporta geolocalização.</p>');
+        return;
+    }
+    navigator.geolocation.getCurrentPosition(sucess, error, {
+        enableHighAccuracy: true //essa propriedade habilitada melhora a precisão da localização
+    });
+    
+    function sucess(position) {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        const spinner = document.querySelector("#spinner");
+        spinner.classList.add("is-active");
+
+        axios
+            .post('/geocode', { lat, lng })
+            .then((response) => {
+                let card = templanteCar(response.data.address, response.data.image);
+                grid.innerHTML += card;
+                spinner.classList.remove("is-active");
+            })
+            .catch((error) => {
+                spinner.classList.remove("is-active");
+            })
+    }
+
+    function error(err) {
+        alert(err);
+    }
+}
